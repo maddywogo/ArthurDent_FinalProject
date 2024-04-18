eca2bd127d6931dde385bade30d1e6fc9e329110
mliso <mliso@DESKTOP-V75G00O.wifi.uc.edu>
2024-04-18 08:20:43 -0400

    Update Functions package to decrypt picture location

---
+++ ArthurDent_FinalProject/functionsPackage/functions.py
@@ -0,0 +1,50 @@
+# functions.py
+# Name: Matthew Lisowsky, Harrison Moore, Maddy Wogomon
+# email: lisowsmd@mail.uc.edu, wogomomr@mail.uc.edu, moorehc@mail.uc.edu
+# Assignment Number: Arthur_Dent_FinalProject
+# Due Date: 4/23/2024
+# Course/Section: IS4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment:
+
+# imports (ex: from functionPackage.functions import *)
+
+
+#ChatGPT used
+def decrypt_location(encrypted_data_file, english_file):
+
+    decrypted_string = ""
+    with open(english_file, "r") as f:
+        english_data = f.readlines()
+    for index in encrypted_data_file:
+        line_number = int(index)
+        decrypted_string += english_data[line_number].strip() + " "
+    return decrypted_string.strip()
+
+# Example usage:
+encrypted_data = [
+
+    "39744",
+    "28799",
+    "27511",
+    "17302",
+    "5082",
+    "31053",
+    "22147",
+    "103568",
+    "21085",
+    "105654",
+    "23926",
+    "23887",
+    "940",
+    "15378",
+    "17514",
+    "41519",
+    "2756",
+    "23926",
+    "23887",
+    "29212"] 
+
+english_file_path = "UCEnglish.txt"
+decrypted_location = decrypt_location(encrypted_data, english_file_path)
+print("Decrypted location string:", decrypted_location)
---
+++ ArthurDent_FinalProject/mainPackage/main.py
@@ -0,0 +1,14 @@
+# main.py.py
+# Name: Matthew Lisowsky, Harrison Moore, Maddy Wogomon
+# email: lisowsmd@mail.uc.edu, wogomomr@mail.uc.edu, moorehc@mail.uc.edu
+# Assignment Number: Arthur_Dent_FinalProject
+# Due Date: 4/23/2024
+# Course/Section: IS4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment:
+
+# imports (ex: from functionPackage.functions import *)
+from functionsPackage.functions import decrypt_location
+
+if __name__ == "__main__":
+    print(decrypt_location("Decrypted location string:", decrypted_location))
